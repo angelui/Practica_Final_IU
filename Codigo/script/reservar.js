02f723aadb2dcac09159f283a2cfa9d1a01079a3
Unknown <ksdd.5192@gmail.com>
2018-12-08 14:53:08 +0100

    slider auto

--- Codigo/script/reservar.js
+++ Codigo/script/reservar.js
@@ -114,56 +114,26 @@ function pagar() {
   window.open("pagar.html","Pagar Reserva")
 }
 
-window.addEventListener('load', function() {
-    console.log('el contenidoha cargado');
-
-    var imagenes =[];
-
-    imagenes[0] = 'img/h1.jpg';
-    imagenes[1] = 'img/h2.jpg';
-    imagenes[2] = 'img/h3.jpg';
-    imagenes[3] = 'img/h4.jpg';
-    imagenes[4] = 'img/h5.jpg';
-
-    var indiceImages = 0;
-    var tiempo = 3000;
-
-    function caambiarImagenes() {
-
-        document.slider.src = imagenes[indiceImages];
-
-        if (indiceImages < 4) {
-            indiceImages++;
-        }else{
-            indiceImages = 0;
-        }
-    }
-
-    setInterval(caambiarImagenes, tiempo);
-
-
-});
 
 window.addEventListener('load', function() {
 
-    var slideIndex = 1;
-    showSlides(slideIndex);
-
+    var slideIndex = 0;
+    showSlides();
     function plusSlides(n) {
         showSlides(slideIndex += n);
     }
+    function showSlides() {
+       var i;
+       var slides = document.getElementsByClassName("mySlides");
+       for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+       }
+       slideIndex++;
+       if(slideIndex > slides.length) {slideIndex = 1}
+       slides[slideIndex-1].style.display = "block";
+       setTimeout(showSlides,2000);
+    }
 
-    function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("dot");
-        if (n > slides.length) {slideIndex = 1}    
-        if (n < 1) {slideIndex = slides.length}
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";  
-        }
-            slides[slideIndex-1].style.display = "active";  
-        }
 });
 
 
